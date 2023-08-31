998dccd11b63c22661315313863e8895b0b43c4e
ss5815190 <ss5815190@gmail.com>
2023-08-31 11:45:39 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
 import RootLayout from './pages/root';
-import AllEventPage from './pages/all-event-page';
+import AllEventPage from './pages/all-event';
 import ErrorPage from './pages/error-page/ErrorPage';
 import CreateEvent from './pages/create-event';
 import Login from './pages/login';
 import Register from './pages/register';
+import Event from './components/Events';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '', element: <Home /> },
-      { path: 'event', element: <AllEventPage /> },
+      { path: 'event', element: <AllEventPage />, loader: Event },
       { path: 'newEvent', element: <CreateEvent /> },
       { path: 'login', element: <Login /> },
       { path: 'register', element: <Register /> },
